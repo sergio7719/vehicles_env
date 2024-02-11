15c44901a3603514e8d4ceeb2b4aec976fa53008
Sergio Berlanga Alvarez <sergio.berlanga@ids.com.mx>
2024-02-10 18:31:38 -0600

    Se agregan los ultimos cambios al codigo

--- app.py
+++ app.py
@@ -1,26 +1,53 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import time
 
-st.header("Vehicles data view")
+st.title(":blue[Vehicles data view]  :car:  🛠️ ")
 
 
       
 vehicles_data = pd.read_csv('notebooks\\vehicles_us.csv') # importacon de los datos
 
-data_state = st.text('Loading data...')
-
-st.write(vehicles_data.head(10))
-
-data_state.text("Done! ")
-
-#Se crea boton 
-if hist_button: 
-    st.write("Histograma para la venta de autos")
+with st.status("Loading data..."):
+        time.sleep(2)
 
 
-#Se crea un histograma
 
-fig = px.histogram(vehicles_data, x = 'odometer')
+# Se crea un boton
 
-st.plotly_chart(fig, use_container_width= True)
+vehicles_info = st.checkbox('information is displayed') 
+
+if vehicles_info:
+        st.write(vehicles_data.head(10))
+
+
+st.button("Return")
+
+
+st.subheader("Car performance information by mileage")
+
+
+# Se crea un boton
+hist_button = st.button('Show histogram') 
+
+if hist_button: # al hacer clic en el botón
+            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+#crear un histograma
+fig = px.histogram(vehicles_data, x="odometer", y = 'type')
+        
+#mostrar un gráfico Plotly interactivo
+st.plotly_chart(fig, use_container_width=True) 
+
+
+st.subheader("price per car model")
+
+price_info = st.button('information is displayed') 
+
+if price_info:
+        st.warning('Price and model scatter plot is created')
+
+
+fig = px.scatter(vehicles_data, x="model", y="price") # crear un gráfico de dispersión
+fig.show() # crear gráfico de dispersión 
